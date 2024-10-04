2303a825dbbc5f7e2cd18f7e65ecc3ba1288b24d
liam waaga <liamgames505@gmail.com>
2024-10-03 18:58:49 -0700

    add rebuild capabilitys to tbuild (pt2)

--- tinybuild/tinybuildConfig.hpp
+++ tinybuild/tinybuildConfig.hpp
@@ -1,13 +1,15 @@
-// All paths are relative tinybuildConfig.hpp
+// All paths are relative "tbuild" executable, ideally placed at project root
 #pragma once
-
-
+// DEFAULT CONFIGURATION
+// MOVE THE TINYBUILD DIRECTORY TO THE ROOT OF THE PROJECT AND THIS FROM THE TINYBUILD DIRECTORY TO THE ROOT OF THE PROJECT
+// FOR MORE INFORMATION GO TO REAMDE.md
+//
+// FOR LICENSE INFORMAITION GO TO LICENSE
+// IF I WAS STUPID AND FORGOT TO PUT A LICENSE, REFER TO https://www.gnu.org/licenses/gpl-3.0.en.html#license-text FOR LICENSE INFORMATION
 #include "defines.hpp"
-
 
 #define BUILDDIR "./build"
 #define EXTENSION "cpp"
-
 
 // build config
 const BuildConfig configs[] = {
@@ -25,6 +27,5 @@ const BuildConfig configs[] = {
         .initFunction=nullptr
     } // add more configurations here    
 };
-
 
 // View defines.hpp for more information (if there isnt the information, too bad)
